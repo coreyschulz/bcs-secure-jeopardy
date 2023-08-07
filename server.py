6ca15dc617b277e7f3419761dbcc1266277b3d75
Corey Schulz <cschulz@hey.com>
2023-08-07 15:34:37 -0600

    Improve error logging on server.

--- server.py
+++ server.py
@@ -13,54 +13,73 @@ async def handle_client(websocket, path):
     global buzz_queue
     global host_socket
     global final_answers
-    clients.add(websocket)
-    username = await websocket.recv()
-    print(f"{username} has connected")
 
-    if username == "host":
-        host_socket = websocket
+    try:
+        clients.add(websocket)
+        username = await websocket.recv()
+        print(f"{username} has connected")
 
-    async for message in websocket:
-        if message == "BUZZ":
-            if buzz_lock and username not in buzz_queue:
-                print(f"{username} buzzed in!")
-                buzz_queue.append(username)
+        if username == "host":
+            host_socket = websocket
+
+        async for message in websocket:
+            if message == "BUZZ":
+                if buzz_lock and username not in buzz_queue:
+                    print(f"{username} buzzed in!")
+                    buzz_queue.append(username)
+                    await update_clients()
+                else:
+                    print(f"{username} buzzed in but was denied!")
+                    await websocket.send("PENALTY")
+
+            elif message == "BOOT":
+                if buzz_queue:
+                    buzz_queue.pop(0)
                 await update_clients()
-            else:
-                print(f"{username} buzzed in but was denied!")
-                await websocket.send("PENALTY")
 
-        elif message == "BOOT":
-            if buzz_queue:
-                buzz_queue.pop(0)
-            await update_clients()
+            elif message == "WIN":
+                if buzz_queue:
+                    win_player = buzz_queue.pop(0)
+                    buzz_queue = []
+                    buzz_lock = False
+                    await update_clients(win_player)
 
-        elif message == "WIN":
-            if buzz_queue:
-                win_player = buzz_queue.pop(0)
-                buzz_queue = []
+            elif message == "LOCK":
                 buzz_lock = False
-                await update_clients(win_player)
+                buzz_queue = []
+                await update_clients()
+            elif message == "UNLOCK":
+                buzz_lock = True
 
-        elif message == "LOCK":
-            buzz_lock = False
-            buzz_queue = []
-            await update_clients()
-        elif message == "UNLOCK":
-            buzz_lock = True
+            elif message == "FINAL":
+                for client in clients:
+                    if client != host_socket:
+                        await client.send("FINAL")
 
-        elif message == "FINAL":
-            for client in clients:
-                if client != host_socket:
-                    await client.send("FINAL")
+            elif message.startswith("FINAL_ANSWER:"):
+                _, answer = message.split(":", 1)
+                final_answers[username] = answer
+                if host_socket:
+                    await host_socket.send(f"FINAL_ANSWER:{username}:{answer}")
 
-        elif message.startswith("FINAL_ANSWER:"):
-            _, answer = message.split(":", 1)
-            final_answers[username] = answer
-            if host_socket:
-                await host_socket.send(f"FINAL_ANSWER:{username}:{answer}")
+    except websockets.ConnectionClosed:
+        print(f"{username} has disconnected")
+    except Exception as e:
+        print(f"Exception: {e}")
 
-    clients.remove(websocket)
+    finally:
+        clients.remove(websocket)
+        close_code = websocket.close_code
+        if close_code == 1000:
+            print(f"{username} has disconnected normally")
+        elif close_code == 1001:
+            print(f"{username} is going away (e.g., server shutdown or browser navigation).")
+        elif close_code == 1002:
+            print(f"{username} disconnected due to protocol error.")
+        elif close_code == 1003:
+            print(f"{username} disconnected due to unsupported data.")
+        else:
+            print(f"{username} disconnected with close code {close_code}.")
 
 async def update_clients(win_player=None):
     if clients:
